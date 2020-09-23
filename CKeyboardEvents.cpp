4f559a6ee51383e90adef53014c0f094c56e1f83
karl wang <754334015@qq.com>
2020-09-23 15:00:46 +0800

    by 木笔

--- CKeyboardEvents.cpp
+++ CKeyboardEvents.cpp
@@ -1,1 +1,81 @@
 #include "CKeyboardEvents.h"
+#include "CGame.h"
+#include <iostream>
+
+CKeyboardEvents* CKeyboardEvents::s_pInstance = 0;
+/*
+* 构造函数
+* 将键值清零
+* @木笔
+*/
+CKeyboardEvents::CKeyboardEvents() : m_keystates(0) {}
+CKeyboardEvents::~CKeyboardEvents()
+{
+    /*删除动态创建的所有内容*/
+    delete m_keystates;
+}
+
+/*
+* 判断键是否按下
+* 参数：key -- 要判断的键
+* @木笔
+*/
+bool CKeyboardEvents::isKeyDown(SDL_Scancode key) const
+{
+    if (m_keystates != 0)
+    {
+        if (m_keystates[key] == 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    return false;
+}
+
+/*
+* 更新按键事件
+* @木笔
+*/
+void CKeyboardEvents::update()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+        switch (event.type)
+        {
+        case SDL_KEYDOWN:
+            onKeyDown();
+            break;
+
+        case SDL_KEYUP:
+            onKeyUp();
+            break;
+
+        default:
+            break;
+        }
+    }
+}
+
+/*
+* 获取键盘按下的键值
+* @木笔
+*/
+void CKeyboardEvents::onKeyDown()
+{
+    m_keystates = SDL_GetKeyboardState(0);
+}
+
+/*
+* 获取键盘弹起的键值
+* @木笔
+*/
+void CKeyboardEvents::onKeyUp()
+{
+    m_keystates = SDL_GetKeyboardState(0);
+}
--- CKeyboardEvents.h
+++ CKeyboardEvents.h
@@ -1,7 +1,51 @@
-#pragma once
+
+#ifndef _CKeyboardEvents_
+#define _CKeyboardEvents_
+
+#include "SDL.h"
 #include "CInputHandle.h"
+
 class CKeyboardEvents :
     public CInputHandle
 {
+public:
+
+    static CKeyboardEvents* Instance()
+    {
+        if (s_pInstance == 0)
+        {
+            s_pInstance = new CKeyboardEvents();
+            s_pInstance->update();
+        }
+
+        return s_pInstance;
+    }
+
+    void update();
+    bool isKeyDown(SDL_Scancode key) const;
+
+    /*用户调用函数判断是否按下键*/
+    static bool ifKeyDown(SDL_Scancode key)
+    {
+        Instance()->update();
+        if (Instance()->isKeyDown(key))
+            return true;
+        else
+            return false;
+    }
+
+private:
+
+    CKeyboardEvents();
+    ~CKeyboardEvents();
+    CKeyboardEvents(const CKeyboardEvents&);
+    void onKeyDown();
+    void onKeyUp();
+
+    const          Uint8* m_keystates;
+    static         CKeyboardEvents* s_pInstance;
 };
 
+
+
+#endif
